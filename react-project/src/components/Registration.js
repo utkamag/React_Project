21ab45b1b0782a9baa4d2c74f62fcd40e02071b3
Егор Вихарев <92789705+utkamag@users.noreply.github.com>
2022-01-14 15:09:52 +0400

    update code

--- react-project/src/components/Registration.js
+++ react-project/src/components/Registration.js
@@ -1,19 +1,23 @@
 import React from "react";
 import {useForm} from "react-hook-form";
+import {useState} from "react";
 
 
 function Registration() {
 
-    const handleSubmit = () => {
-        console.log("123")
+    const {register, handleSubmit} = useForm()
+    const {name, setName} = useState("123")
+
+    const qiwi = (data) => {
+        setName(JSON.stringify(data))
 
     }
 
     return (
         <div className="Registration">
             <h1>Регистрация</h1>
-            <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Введите "/>
+            <form onSubmit={handleSubmit(qiwi)}>
+                <input type="text" placeholder="Введите " {...register("name")}/>
 
                 <button type="submit">Готово!</button>
 
